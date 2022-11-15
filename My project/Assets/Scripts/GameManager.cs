2c734cf95c33f334f4be5bc088280fa357d69a4e
Satori <3102809947@qq.com>
2022-11-16 00:38:31 +0800

    upd:get_action_attack_range_path

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -71,4 +71,372 @@ public class GameManager : MonoBehaviour
 		characterInfoButton.SetActive(false);
 		placeInfoButton.SetActive(false);
 	}
+
+	private bool CheckAccessible(Vector3Int position)
+	{
+		var x = position.x;
+		var y = position.y;
+		
+		if (x < 0 || x >= DataManager.MapSize || y < 0 || y >= DataManager.MapSize)
+		{
+			return false;
+		}
+
+		if (DataManager.Instance.GetCharacterByPosition(position) != null)
+		{
+			return false;
+		}
+
+		if (DataManager.Instance.GetStructureByPosition(position).player.id != DataManager.Instance.currentPlayer.id)
+		{
+			return false;
+		}
+
+		return true;
+	}
+	
+	
+	private static bool CheckBound(Vector3Int position)
+	{
+		var x = position.x;
+		var y = position.y;
+		return !(x < 0 || x >= DataManager.MapSize || y < 0 || y >= DataManager.MapSize);
+	}
+
+	private bool CheckAttack(Vector3Int position)
+	{
+		if (DataManager.Instance.GetCharacterByPosition(position).player.id != DataManager.Instance.currentPlayer.id)
+		{
+			return true;
+		}
+
+		if (DataManager.Instance.GetStructureByPosition(position).player == null ||
+		    DataManager.Instance.GetStructureByPosition(position).player.id != DataManager.Instance.currentPlayer.id)
+		{
+			return true;
+		}
+
+		return false;
+	}
+	
+	private List<Vector3Int> GetAttackRange(Vector3Int position)
+	{
+		var character = DataManager.Instance.GetCharacterByPosition(position);
+		if (character == null)
+		{
+			return default;
+		}
+
+		if (character.player.id != DataManager.Instance.currentPlayer.id || character.hp <= 0)
+		{
+			return default;
+		}
+
+		if (character.actionState >= 2)
+		{
+			// can't attack
+			return default;
+		}
+
+		var n = DataManager.MapSize;
+		var actionRange = character.actionRange;
+		var result = new List<Vector3Int>();
+		var dis = new int[n,n];
+		for (var i = 0; i < n; i++)
+		{
+			for (var j = 0; j < n; j++)
+			{
+				dis[i,j] = 0;
+			}
+		}
+		dis[position.x,position.y] = 1;
+		for (var t = 1; t < actionRange; t++)
+		for (var i = 0; i < n; i++)
+		for (var j = 0; j < n; j++)
+			if (dis[i, j] == t)
+			{
+				if (CheckBound(new Vector3Int(i, j + 1, position.z)))
+				{
+					if (dis[i, j + 1] == 0)
+					{
+						dis[i, j + 1] = t + 1;
+					}
+				}
+
+				if (CheckBound(new Vector3Int(i, j - 1, position.z)))
+				{
+					if (dis[i, j - 1] == 0)
+					{
+						dis[i, j - 1] = t + 1;
+					}
+				}
+
+				if (CheckBound(new Vector3Int(i + 1, j, position.z)))
+				{
+					if (dis[i + 1, j] == 0)
+					{
+						dis[i + 1, j] = t + 1;
+					}
+				}
+
+				if (CheckBound(new Vector3Int(i - 1, j, position.z)))
+				{
+					if (dis[i - 1, j] == 0)
+					{
+						dis[i - 1, j] = t + 1;
+					}
+				}
+
+
+				if (CheckBound(new Vector3Int(i + 1, j - 1, position.z)))
+				{
+					if (dis[i + 1, j] == 0)
+					{
+						dis[i + 1, j] = t + 1;
+					}
+				}
+
+				if (CheckBound(new Vector3Int(i - 1, j - 1, position.z)))
+				{
+					if (dis[i - 1, j] == 0)
+					{
+						dis[i - 1, j] = t + 1;
+					}
+				}
+			}
+
+		for (var i = 0; i < n; i++)
+		{
+			for (var j = 0; j < n; j++)
+			{
+				if (dis[i, j] != 0 && CheckAttack(new Vector3Int(i,j,position.z)))
+				{
+					result.Add(new Vector3Int(i,j,position.z));
+				}
+			}
+		}
+
+		return result;
+	}
+	
+	private List<Vector3Int> GetActionRange(Vector3Int position)
+	{
+		var character = DataManager.Instance.GetCharacterByPosition(position);
+		if (character == null)
+		{
+			return default;
+		}
+
+		if (character.player.id != DataManager.Instance.currentPlayer.id || character.hp <= 0)
+		{
+			return default;
+		}
+
+		if (character.actionState >= 1)
+		{
+			// can't move
+			return default;
+		}
+
+		var n = DataManager.MapSize;
+		var actionRange = character.actionRange;
+		var result = new List<Vector3Int>();
+		var dis = new int[n,n];
+		for (var i = 0; i < n; i++)
+		{
+			for (var j = 0; j < n; j++)
+			{
+				dis[i,j] = 0;
+			}
+		}
+		dis[position.x,position.y] = 1;
+		for (var t = 1; t < actionRange; t++)
+		for (var i = 0; i < n; i++)
+		for (var j = 0; j < n; j++) 
+			if (dis[i, j] == t)
+			{
+				if (CheckAccessible(new Vector3Int(i,j+1,position.z)))
+				{
+					if (dis[i, j + 1] == 0)
+					{
+						dis[i, j + 1] = t + 1;
+					}
+				}	
+				
+				if (CheckAccessible(new Vector3Int(i,j-1,position.z)))
+				{
+					if (dis[i, j - 1] == 0)
+					{
+						dis[i, j - 1] = t + 1;
+					}
+				}	
+
+				if (CheckAccessible(new Vector3Int(i+1,j,position.z)))
+				{
+					if (dis[i+1, j] == 0)
+					{
+						dis[i+1, j] = t + 1;
+					}
+				}	
+				
+				if (CheckAccessible(new Vector3Int(i-1,j,position.z)))
+				{
+					if (dis[i-1, j] == 0)
+					{
+						dis[i-1, j] = t + 1;
+					}
+				}
+				
+				
+				if (CheckAccessible(new Vector3Int(i+1,j-1,position.z)))
+				{
+					if (dis[i+1, j] == 0)
+					{
+						dis[i+1, j] = t + 1;
+					}
+				}	
+				
+				if (CheckAccessible(new Vector3Int(i-1,j-1,position.z)))
+				{
+					if (dis[i-1, j] == 0)
+					{
+						dis[i-1, j] = t + 1;
+					}
+				}
+			}
+
+		for (var i = 0; i < n; i++)
+		{
+			for (var j = 0; j < n; j++)
+			{
+				if (dis[i, j] != 0)
+				{
+					result.Add(new Vector3Int(i,j,position.z));
+				}
+			}
+		}
+
+		return result;
+	}
+	
+	private List<Vector3Int> GetActionPath(Vector3Int position,Vector3Int target)
+	{
+		var character = DataManager.Instance.GetCharacterByPosition(position);
+		if (!CheckAccessible(target))
+		{
+			return default;
+		}
+
+		var n = DataManager.MapSize;
+		var actionRange = character.actionRange;
+		var result = new List<Vector3Int>();
+		var dis = new int[n,n];
+		for (var i = 0; i < n; i++)
+		{
+			for (var j = 0; j < n; j++)
+			{
+				dis[i,j] = 0;
+			}
+		}
+		dis[position.x,position.y] = 1;
+		for (var t = 1; t < actionRange; t++)
+		for (var i = 0; i < n; i++)
+		for (var j = 0; j < n; j++) 
+			if (dis[i, j] == t)
+			{
+				if (CheckAccessible(new Vector3Int(i,j+1,position.z)))
+				{
+					if (dis[i, j + 1] == 0)
+					{
+						dis[i, j + 1] = t + 1;
+					}
+				}	
+				
+				if (CheckAccessible(new Vector3Int(i,j-1,position.z)))
+				{
+					if (dis[i, j - 1] == 0)
+					{
+						dis[i, j - 1] = t + 1;
+					}
+				}	
+
+				if (CheckAccessible(new Vector3Int(i+1,j,position.z)))
+				{
+					if (dis[i+1, j] == 0)
+					{
+						dis[i+1, j] = t + 1;
+					}
+				}	
+				
+				if (CheckAccessible(new Vector3Int(i-1,j,position.z)))
+				{
+					if (dis[i-1, j] == 0)
+					{
+						dis[i-1, j] = t + 1;
+					}
+				}
+				
+				
+				if (CheckAccessible(new Vector3Int(i+1,j-1,position.z)))
+				{
+					if (dis[i+1, j] == 0)
+					{
+						dis[i+1, j] = t + 1;
+					}
+				}	
+				
+				if (CheckAccessible(new Vector3Int(i-1,j-1,position.z)))
+				{
+					if (dis[i-1, j] == 0)
+					{
+						dis[i-1, j] = t + 1;
+					}
+				}
+			}
+
+		if (dis[target.x, target.y] == 0)
+		{
+			//can't
+			return default;
+		}
+
+		var x = target.x;
+		var y = target.y;
+		result.Add(target);
+		for (var t = dis[x, y]-1; t >=1; t--)
+		{
+			if (dis[x, y - 1] == t)
+			{
+				y -= 1;
+			}
+			else if (dis[x, y + 1] == t)
+			{
+				y += 1;
+			}
+			else if (dis[x+1, y - 1] == t)
+			{
+				x += 1;
+				y -= 1;
+			}
+			else if (dis[x-1, y - 1] == t)
+			{
+				x -= 1;
+				y -= 1;
+			}
+			else if (dis[x+1, y] == t)
+			{
+				x += 1;
+			}
+			else if (dis[x-1, y] == t)
+			{
+				x -= 1;
+			}
+			result.Add(new Vector3Int(x,y,target.z));
+		}
+		result.Reverse();
+
+		return result;
+	}
+
+	
 }
