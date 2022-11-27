0c24c1f5f0682a4c4d94c9b928f1c6f2acb37f9a
Tavish <12011618@mail.sustech.edu.cn>
2022-11-28 00:13:05 +0800

    update structure listener

--- My project/Assets/Scripts/PlaceInfoFrame.cs
+++ My project/Assets/Scripts/PlaceInfoFrame.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using Model;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,8 +10,15 @@ public class PlaceInfoFrame : MonoBehaviour
 {
     public Image structureImage;
     public TextMeshProUGUI structureName;
+    public Button function;
     public Slider hp;
     private Vector3Int _position;
+
+    private void OnEnable()
+    {
+        function = function.GetComponent<Button>();
+        RenderData(_position);
+    }
 
     public void Inform(Vector3Int position)
     {
@@ -21,5 +30,32 @@ public class PlaceInfoFrame : MonoBehaviour
         var structure = DataManager.Instance.GetStructureByPosition(position);
         structureName.text = structure.structureClass.ToString();
         hp.value = structure.hp;
+        string side;
+        if (DataManager.Instance.CheckStructureSide(structure) == -1)
+            side = "blue";
+        else if (DataManager.Instance.CheckStructureSide(structure) == 1)
+            side = "red";
+        else
+            side = "middle";
+        structureImage.sprite = RenderManager.Instance.GetStructureImage(structure.structureClass, side);
+        function.enabled = true;
+        switch (structure.structureClass)
+        {
+            case StructureClass.CAMP:
+                function.GetComponent<TextMeshProUGUI>().text= "train";
+                function.onClick.AddListener(GameManager.Instance.OpenCamp);
+                break;
+            case StructureClass.MARKET:
+                function.GetComponent<TextMeshProUGUI>().text = "shop";
+                function.onClick.AddListener(GameManager.Instance.OpenShop);
+                break;
+            case StructureClass.INSTITUTE:
+                function.GetComponent<TextMeshProUGUI>().text = "Tech";
+                function.onClick.AddListener(GameManager.Instance.OpenTechnologies);
+                break;
+            case StructureClass.VILLAGE:
+                function.enabled = false;
+                break;
+        }
     }
 }
