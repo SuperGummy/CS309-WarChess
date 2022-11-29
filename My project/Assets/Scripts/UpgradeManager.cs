77bf5df5ad77ec914b6572ab2010b2202834351a
seaflowery <12012940@mail.sustech.edu.cn>
2022-11-29 16:54:00 +0800

    finish upgrade manager

--- My project/Assets/Scripts/UpgradeManager.cs
+++ My project/Assets/Scripts/UpgradeManager.cs
@@ -1,1 +1,42 @@
-﻿
+﻿using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpgradeManager: MonoBehaviour
+{
+    public Button[] upgradeBtn;
+    public GameObject upgradeFrame;
+
+    public void Start()
+    {
+        upgradeFrame.SetActive(true);
+        foreach (Button btn in upgradeBtn)
+        {
+            btn.onClick.AddListener(delegate { OnClickUpgrade(btn); });
+        }
+    }
+
+    public void OnClickUpgrade(Button btn)
+    {
+        int type = -1;
+        switch (btn.name)
+        {
+            case "UpgradeCollege":
+                type = 2;
+                break;
+            case "UpgradeCamp":
+                type = 0;
+                break;
+            case "UpgradeMarket":
+                type = 1;
+                break;
+            default:
+                throw new Exception("Cannot find " + btn.name);
+        }
+        
+        upgradeFrame.SetActive(false);
+        
+        GameManager.Instance.UpgradeStructure(type);
+    }
+}
