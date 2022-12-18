8b680328cf97fc21287de702ba7334002b5b90d6
Shuxin <1902605778@qq.com>
2022-12-18 20:43:33 +0800

    Fix bug in load

--- My project/Assets/Scripts/ProgressControl/ProgressReportModel.cs
+++ My project/Assets/Scripts/ProgressControl/ProgressReportModel.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ProgressReportModel
+public class ProgressReportModel : MonoBehaviour
 {
     public int progressValue;
 }
